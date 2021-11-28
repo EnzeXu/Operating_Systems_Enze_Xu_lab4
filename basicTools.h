dc5b8247ecba516a0fefaac1be9e96e7f4b1d5ff
EnzeXu <xue20@wfu.edu>
2021-11-28 16:58:38 -0500

    test

--- basicTools.h
+++ basicTools.h
@@ -12,9 +12,9 @@
 #include <sys/wait.h>
 #include <error.h>
 
-#define PROJ 0x2021
-#define PROJ_SEM_1 0x0001
-#define PROJ_SEM_2 0x0002
+#define PROJ_MSG 0x0001
+#define PROJ_SEM_1 0x0002
+#define PROJ_SEM_2 0x0003
 #define REQUEST 0
 #define REPLY 1
 #define MAXSIZE 128
