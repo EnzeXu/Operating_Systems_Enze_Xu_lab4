fabf80c9136486bfff43efbab82eb449bc449b84
EnzeXu <xue20@wfu.edu>
2021-11-28 17:52:09 -0500

    test

--- print_server.c
+++ print_server.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
 	int semid = createSemid(2, PROJ_SEM_1);
 	// int semid_2 = createSemid(1, PROJ_SEM_2);
 	printf("creating semid_1 = %d successfully\n", semid_1);
-	printf("creating semid_2 = %d successfully\n", semid_2);
+	//printf("creating semid_2 = %d successfully\n", semid_2);
 	removeMessageQueue(msgid);
 	system("/usr/bin/ipcs");
 	removeSem(semid_1, 0);
