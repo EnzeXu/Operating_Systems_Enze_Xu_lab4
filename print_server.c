2196b50eb048afe48c9e8f766b784d9a370f437b
EnzeXu <xue20@wfu.edu>
2021-11-28 17:51:31 -0500

    test

--- print_server.c
+++ print_server.c
@@ -8,13 +8,15 @@ int main(int argc, char *argv[]) {
 	printf("Print server is node %d\n", me);
 	int msgid = createMessageQueue(PROJ_MSG);
 	printf("creating msgid = %d successfully\n", msgid);
-	int semid_1 = createSemid(1, PROJ_SEM_1);
-	int semid_2 = createSemid(1, PROJ_SEM_2);
+	int semid = createSemid(2, PROJ_SEM_1);
+	// int semid_2 = createSemid(1, PROJ_SEM_2);
 	printf("creating semid_1 = %d successfully\n", semid_1);
 	printf("creating semid_2 = %d successfully\n", semid_2);
 	removeMessageQueue(msgid);
+	system("/usr/bin/ipcs");
 	removeSem(semid_1, 0);
-	sleep(2);
-	removeSem(semid_2, 0);
+	system("/usr/bin/ipcs");
+	removeSem(semid_1, 1);
+	system("/usr/bin/ipcs");
 	return 0;
 }
