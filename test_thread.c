c0cae4895be4806fd4b8b53c030ba85f780f8440
EnzeXu <xue20@wfu.edu>
2021-11-28 22:50:01 -0500

    test

--- test_thread.c
+++ test_thread.c
@@ -3,11 +3,18 @@
 
 int k;
 
+struct threadParam {
+	int node;
+	int x;
+}
+
 void *fun(void *arg) {
-	int node = *(int *)arg;
+	//int node = *(int *)arg;
+	struct threadParam *pt;
+	pt = (struct threadParam *) arg;
 	for (int i = 0; i < 10; ++i) {
 		k++;
-		printf("%d: %d\n", node, k);
+		printf("%d(%d): %d\n", pt->node, pt->x, k);
 		usleep(1);
 	}
 	return (void *)0;
@@ -17,9 +24,16 @@ int main() {
 	pthread_t thread1, thread2, thread3;
 	int a = 1, b = 2, c = 3;
 	k = 0;
-	pthread_create(&thread1, NULL, fun, &a);
-	pthread_create(&thread2, NULL, fun, &b);
-	pthread_create(&thread3, NULL, fun, &c);
+	struct threadParam tp1, tp2, tp3;
+	tp1.node = 1;
+	tp1.x = 111;
+	tp2.node = 2;
+	tp2.x = 222;
+	tp3.node = 3;
+	tp3.x = 333;
+	pthread_create(&thread1, NULL, fun, &tp1);
+	pthread_create(&thread2, NULL, fun, &tp2);
+	pthread_create(&thread3, NULL, fun, &tp3);
 	/*
 	if (pthread_create(&thread1, NULL, fun, NULL) < 0) {
 		perror("pthread_create");
