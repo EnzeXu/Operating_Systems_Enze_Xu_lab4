6705af6a68e745bdec5357078d597e5dabcf8209
EnzeXu <xue20@wfu.edu>
2021-11-28 22:24:38 -0500

    test

---
+++ test_thread.c
@@ -0,0 +1,26 @@
+#include "messageTools.h"
+#include "semaphoreTools.h"
+
+int fun() {
+	for (int i = 0; i < 10; ++i) {
+		print("%d ", i);
+		usleep(10000);
+	}
+	return 0;
+}
+
+int main() {
+	pthread_t thread1, thread2;
+	if (pthread_create(&thread1, NULL, fun, NULL) < 0) {
+		perror("pthread_create");
+		exit(1);
+	}
+	if (pthread_create(&thread2, NULL, fun, NULL) < 0) {
+		perror("pthread_create");
+		exit(1);
+	}
+	pthread_join(thread1, NULL)
+	pthread_join(thread2, NULL)
+	printf("finished!\n");
+	return 0;
+}
