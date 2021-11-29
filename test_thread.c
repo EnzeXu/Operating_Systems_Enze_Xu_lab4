70fcc83d9afb1f1092ad86350112733cb8c6b0b9
EnzeXu <xue20@wfu.edu>
2021-11-28 22:42:06 -0500

    test

--- test_thread.c
+++ test_thread.c
@@ -6,8 +6,9 @@ int k;
 void *fun(void *arg) {
 	int node = *(int *)arg;
 	for (int i = 0; i < 10; ++i) {
-		printf("%d: %d\n", node, i);
-		usleep(1);
+		k++;
+		printf("%d: %d\n", node, k);
+		//usleep(1);
 	}
 	return (void *)0;
 }
@@ -15,6 +16,7 @@ void *fun(void *arg) {
 int main() {
 	pthread_t thread1, thread2, thread3;
 	int a = 1, b = 2, c = 3;
+	k = 0;
 	pthread_create(&thread1, NULL, fun, &a);
 	pthread_create(&thread2, NULL, fun, &b);
 	pthread_create(&thread3, NULL, fun, &c);
