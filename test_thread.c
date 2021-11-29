d7e58f02ebc8cd63cc73ef067945e5059430e58e
EnzeXu <xue20@wfu.edu>
2021-11-28 22:34:46 -0500

    test

--- test_thread.c
+++ test_thread.c
@@ -4,13 +4,13 @@
 void *fun() {
 	for (int i = 0; i < 10; ++i) {
 		printf("%d ", i);
-		usleep(10000);
+		//usleep(10000);
 	}
-	return ((void *)0);
+	return (void *)0;
 }
 
 int main() {
-	pthread_t thread1, thread2;
+	pthread_t thread1, thread2, thread3;
 	if (pthread_create(&thread1, NULL, fun, NULL) < 0) {
 		perror("pthread_create");
 		exit(1);
@@ -19,8 +19,13 @@ int main() {
 		perror("pthread_create");
 		exit(1);
 	}
+	if (pthread_create(&thread3, NULL, fun, NULL) < 0) {
+		perror("pthread_create");
+		exit(1);
+	}
 	pthread_join(thread1, NULL);
 	pthread_join(thread2, NULL);
+	pthread_join(thread3, NULL);
 	printf("finished!\n");
 	return 0;
 }
