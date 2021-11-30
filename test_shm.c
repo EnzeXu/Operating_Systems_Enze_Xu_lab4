3a79f1589f9aeccf23bb038fdcf4c949cdaaf6f2
EnzeXu <xue20@wfu.edu>
2021-11-29 21:41:52 -0500

    cool

--- test_shm.c
+++ test_shm.c
@@ -1,12 +1,11 @@
 #include "sharedMemoryTools.h"
 
 int main() {
-	int shmid;
 	char *addr;
 	int i = 0;
-	shmid = GetShm(1024, 0x0001);
+	int shmid = GetShm(1024, 0x0001);
 	addr = shmat(shmid, NULL, 0);
-	
+	printf("checkpoint1\n");
 	pid_t pid1 = fork();
 	if (pid1 == 0) {
 		/*
@@ -14,13 +13,12 @@ int main() {
 			printf("");
 		}
 		*/
+		printf("checkpoint2\n");
 		int shmid;
 		char *addr;
 		int i = 0;
-		
-		
-		shmid = GetShm(1024, 0x0001);
-		addr = shmat(shmid, NULL, 0);
+		//shmid = GetShm(1024, 0x0001);
+		//addr = shmat(shmid, NULL, 0);
 		int z = 10;
 		while (z--) {
 			addr[i] = 'Z' - i;
@@ -40,17 +38,18 @@ int main() {
 		return 0;
 	}
 	*/
+	printf("checkpoint3\n");
 	int z = 10;
 	while (z--) {
 		printf("parent: %s\n", addr);
 		sleep(1);
 	}
-	
+	printf("checkpoint4\n");
 	shmdt(addr);
 	int status1, status2; 
 	waitpid(pid1, &status1, 0); 
 	DestroyShm(shmid);
-	
+	printf("checkpoint5\n");
 	//waitpid(pid2, &status2, 0); 
 	return 0;
 }
